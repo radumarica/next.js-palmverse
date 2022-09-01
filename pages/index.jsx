fcc617fcb54a90e78165530603f9eb7c708832de
riz <rizwan.k.off@gmail.com>
2022-09-01 22:04:03 +0530

    tesst

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,25 @@
 import Hero from "../components/Page/Home/Hero";
+import { HiArrowNarrowRight } from 'react-icons/hi'
+import Link from "next/link";
 
 const Homepage = () => (
   <>
-    <video
-      src="/videos/home3.mp4"
-      loop
-      autoPlay
-      muted
-      className="md:h-[560px] object-cover  w-full "
-    />
+    <div className="relative">
+
+      <video
+        src="/videos/home3.mp4"
+        loop
+        autoPlay
+        muted
+        className="md:h-[90vh] object-cover  w-full "
+      />
+      <Link href="/who-we-are">
+        <div className="absolute z-20 bottom-2 md:bottom-5 md:left-10 left-5 flex items-center cursor-pointer hover:pl-5 transition-all" >
+          <HiArrowNarrowRight className="md:text-4xl text-xl  text-white  " />
+          <p className="text-white font-exo font-bold text-xl md:text-3xl pl-4">Discover Our Story</p>
+        </div>
+      </Link>
+    </div>
 
     <Hero />
   </>
