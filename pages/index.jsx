7b79374945e5aaf9df13aec5a58277aae30e4b6a
riz <rizwan.k.off@gmail.com>
2022-09-01 22:10:36 +0530

    test

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,9 +14,9 @@ const Homepage = () => (
         className="md:h-[90vh] object-cover  w-full "
       />
       <Link href="/who-we-are">
-        <div className="absolute z-20 bottom-2 md:bottom-5 md:left-10 left-5 flex items-center cursor-pointer hover:pl-5 transition-all" >
-          <HiArrowNarrowRight className="md:text-4xl text-xl  text-white  " />
-          <p className="text-white font-exo font-bold text-xl md:text-3xl pl-4">Discover Our Story</p>
+        <div className="absolute z-20 bottom-2 md:bottom-8 md:left-10 left-5 flex items-center cursor-pointer hover:pl-5 transition-all" >
+          <HiArrowNarrowRight className="md:text-6xl text-xl  text-white pt-2  " />
+          <p className="text-white font-exo font-bold text-xl md:text-5xl md:tracking-wider pl-4">Discover Our Story</p>
         </div>
       </Link>
     </div>
