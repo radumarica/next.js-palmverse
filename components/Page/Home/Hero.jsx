3b088fd2c3cfbea79de99b3807eb9023d8b1f39d
riz <rizwan.k.off@gmail.com>
2022-09-03 00:39:04 +0530

    test

--- components/Page/Home/Hero.jsx
+++ components/Page/Home/Hero.jsx
@@ -30,7 +30,7 @@ const secondSetIcons = [
     },
     {
         link: '/icons/hand.png',
-        title: 'Profits Added to DAO treasury. Holders Rewarded'
+        title: `Profits Added to DAO treasury. Holders Rewarded`
     },
     {
         link: '/icons/coin.png',
@@ -185,15 +185,27 @@ const Hero = () => {
                 <div className="flex flex-col">
                     <h1 className="text-3xl font-bold text-[#ECF7FD]  text-center py-6">
                         NFT UTILITIES - TRAVEL 2 EARN</h1>
-                    <div className="grid grid-cols-1  gap-4 py-4">
-                        {
-                            secondSetIcons.map((icon, index) => (
-                                <div key={icon.link} className={`flex justify-between items-center ${index % 2 === 0 && 'flex-row-reverse'}`}>
-                                    <Image src={icon.link} alt={icon.link} height={150} width={200} layout={"fixed"} />
-                                    <h1 className="md:text-[26px] text-[20px] text-[#ECF7FD] font-medium w-[90%] text-center py-2">{icon.title}</h1>
-                                </div>
-                            ))
-                        }
+                    <div className="grid grid-cols-1  gap-8 py-4">
+
+
+                        <div className={`flex flex-row-reverse justify-between items-center  `}>
+                            <Image src='/icons/tag.png' alt='tag' height={140} width={180} layout={"fixed"} />
+                            <h1 className="md:text-[26px] text-[24px] font-semibold text-[#ECF7FD]  w-[90%] text-center py-2">Up to 30% Discounts for  <br /> Rooms & Drinks</h1>
+                        </div>
+                        <div className={`flex  justify-between items-center  `}>
+                            <Image src='/icons/hand.png' alt='tag' height={140} width={180} layout={"fixed"} />
+                            <h1 className="md:text-[26px] text-[24px] font-semibold text-[#ECF7FD]  w-[90%] text-center py-2">Profits Added to <br /> DAO treasury. <br /> Holders Rewarded</h1>
+                        </div>
+                        <div className={`flex flex-row-reverse justify-between items-center  `}>
+                            <Image src='/icons/coin.png' alt='tag' height={140} width={180} layout={"fixed"} />
+                            <h1 className="md:text-[26px] text-[24px] font-semibold text-[#ECF7FD]  w-[90%] text-center py-2">Seed Tokens  <br /> Allocation</h1>
+                        </div>
+                        <div className={`flex  justify-between items-center  `}>
+                            <Image src='/icons/cycle.png' alt='tag' height={140} width={180} layout={"fixed"} />
+                            <h1 className="md:text-[26px] text-[24px] font-semibold text-[#ECF7FD]  w-[90%] text-center py-2">Cashback Travel Rewards</h1>
+                        </div>
+
+
                     </div>
                 </div>
                 <div className="flex flex-col justify-end items-center py-4 md:py-0" >
