0fca914f68ecc6c0a1a1654b035f50bb3539d934
riz <rizwan.k.off@gmail.com>
2022-09-04 19:18:56 +0530

    test

--- components/Page/Home/Hero.jsx
+++ components/Page/Home/Hero.jsx
@@ -102,8 +102,8 @@ const Hero = () => {
                     </h1>
                 </div>
             </div>
-            <div className="flex flex-col items-center justify-center py-16 space-y-10 md:space-y-0" >
-                <div className="text-3xl relative  md:w-[60%] mx-auto text-center text-[#ECF7FD] pb-4 "><div className="absolute z-50 -top-2 -left-2" >
+            <div className="flex flex-col items-center justify-center py-10 space-y-10 md:space-y-0" >
+                <div className="text-3xl relative  md:w-[60%] mx-auto text-center text-[#ECF7FD] pb-4 "><div className="absolute z-50 -top-2 left-0" >
                     <Image src='/images/palm.png' alt="palm" height="45" width="35" />
                 </div><p>We refurbish old buildings and leverage our unique property conversion model to turn them into the Palmverse brand. </p></div>
                 <div className="scale-110 md:scale-100">
