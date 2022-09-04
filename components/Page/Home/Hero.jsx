87df8e5b8e26c4ec63eefdc91cd3e19613473356
riz <rizwan.k.off@gmail.com>
2022-09-04 22:19:34 +0530

    test

--- components/Page/Home/Hero.jsx
+++ components/Page/Home/Hero.jsx
@@ -161,16 +161,18 @@ const Hero = () => {
                     </div>
 
                 </div>
-                <h1 className="md:text-3xl text-2xl text-center font-bold text-[#ECF7FD]">Whether you’re a travel addict, digital nomad, <span className="text-green_text">an NFT wanderer</span>, or a surfer looking for paradise, <span className="underline underline-offset-4" >you’ve come to the right place.</span></h1>
+                <h1 className="md:text-3xl text-[18px] text-center font-light leading-7 text-[#ECF7FD]">Whether you’re a travel addict, digital nomad, <span className="text-green_text">an NFT wanderer</span>, or a surfer looking for paradise, <span className="underline underline-offset-4" >you’ve come to the right place.</span></h1>
             </div>
             <div className="   md:mt-0 md:scale-100">
 
                 <Image src='/images/laptop.png' alt='laptop palverse' width={550} height={370} />
             </div>
-            <h1 className="text-3xl font-medium text-center py-4 text-[#ECF7FD]">
+            <div className="bg-[#ECF7FD] text-black py-2 my-2">
 
-                FUNCTIONAL BOOKING PLATFORM
-            </h1>
+                <h1 className="text-[28px] font-medium  text-center ">
+                    FUNCTIONAL BOOKING PLATFORM
+                </h1>
+            </div>
             <div className="grid md:grid-cols-2 gap-4 py-16 " >
                 <div className="flex flex-col">
                     <h1 className="text-3xl font-bold text-[#ECF7FD]  text-center py-6">
