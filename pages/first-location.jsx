7693b879a6c23d820b3b1c1e035ca67a9d84f566
riz <rizwan.k.off@gmail.com>
2022-09-05 23:32:51 +0530

    test

--- pages/first-location.jsx
+++ pages/first-location.jsx
@@ -11,10 +11,10 @@ const FirstLocation = () => {
             <div className="md:w-[80rem] mx-auto">
 
 
-                <p className="text-[18px] leading-[22px] font-medium py-1 px-3 md:text-left pt-4">We rent and refurbish old properties, converting them into the Palmverse brand model. </p>
-                <p className="text-[18px] leading-[22px] font-medium py-1 px-3 md:text-left">With this business plan, we can achieve a quick global expansion, keeping the rent expense of the properties low by refurbishing the buildings to meet the Palmverse standards.</p>
-                <p className="text-[18px] leading-[22px] font-medium py-1 px-3 md:text-left">The first Palmverser Hotel & Hostel will have approx. 17 rooms (private and shared) with co-working space, a pool, and a bar.</p>
-                <p className="text-[18px] leading-[22px] font-medium py-1 px-3 md:text-left">We aim to bridge the web2 booking experience to web3, by accepting On-chain cryptocurrency payments to book stays in the first NFT Hotel in Costa Rica.</p>
+                <p className="text-[18px] leading-[21px] font-medium py-1 px-3 md:text-left pt-4">We rent and refurbish old properties, converting them into the Palmverse brand model. </p>
+                <p className="text-[18px] leading-[21px] font-medium py-1 px-3 md:text-left">With this business plan, we can achieve a quick global expansion, keeping the rent expense of the properties low by refurbishing the buildings to meet the Palmverse standards.</p>
+                <p className="text-[18px] leading-[21px] font-medium py-1 px-3 md:text-left">The first Palmverser Hotel & Hostel will have approx. 17 rooms (private and shared) with co-working space, a pool, and a bar.</p>
+                <p className="text-[18px] leading-[21px] font-medium py-1 px-3 md:text-left">We aim to bridge the web2 booking experience to web3, by accepting On-chain cryptocurrency payments to book stays in the first NFT Hotel in Costa Rica.</p>
                 <div className="pt-10">
                     <div className="relative  mx-auto bg-[#ECF7FD] text-black w-screen">
 
@@ -41,14 +41,14 @@ const FirstLocation = () => {
                     <div className="text-[21px] relative     font-medium mt-4  text-[#ECF7FD]  "><div className="absolute z-50 -top-5 left-0 lg:-left-3" >
 
                     </div><p className="pl-4"><span className="w-4 "></span>  Costa Rica was chosen as the first location considering multiple factors such as the year-round season and good market prices for hotel rooms. </p></div>
-                    <p className="text-[21px] font-medium py-3 px-3 md:text-left  ">Secondly, Costa Rica Is considered a  cryptocurrency paradise due to the widespread adoption of cryptocurrencies by its citizens. Many stores accept cryptocurrency as payment</p>
-                    <p className="text-[21px] font-medium py-2 px-3 md:text-left  ">We will implement cryptocurrency payments at the front desk and on our website. </p>
-                    <p className="text-[21px] font-medium py-2 px-3 md:text-left  ">Usual activities in Costa Rica are surfing, rafting, sailing, ATV tours, volcano tours, natural parks visits, hiking, turtle watching, horseriding, and many more. </p>
-                    <p className="text-[21px] font-medium py-2 px-3 md:text-left  ">Possible 2nd and 3rd Palmverse locations, which are also crypto-friendly :</p>
+                    <p className="text-[18px] leading-[22px] font-medium py-3 px-3 md:text-left  ">Secondly, Costa Rica Is considered a  cryptocurrency paradise due to the widespread adoption of cryptocurrencies by its citizens. Many stores accept cryptocurrency as payment</p>
+                    <p className="text-[18px] leading-[22px] font-medium py-2 px-3 md:text-left  ">We will implement cryptocurrency payments at the front desk and on our website. </p>
+                    <p className="text-[18px] leading-[22px] font-medium py-2 px-3 md:text-left  ">Usual activities in Costa Rica are surfing, rafting, sailing, ATV tours, volcano tours, natural parks visits, hiking, turtle watching, horseriding, and many more. </p>
+                    <p className="text-[18px] leading-[22px] font-medium py-2 px-3 md:text-left  ">Possible 2nd and 3rd Palmverse locations, which are also crypto-friendly :</p>
                     <ul className="pl-8" >
-                        <li className="list-disc py-1 text-[21px] font-bold">Miami</li>
-                        <li className="list-disc py-1 text-[21px] font-bold">Portugal</li>
-                        <li className="list-disc py-1 text-[21px] font-bold">Panama</li>
+                        <li className="list-disc py-1 text-[18px] leading-[22px] font-bold">Miami</li>
+                        <li className="list-disc py-1 text-[18px] leading-[22px] font-bold">Portugal</li>
+                        <li className="list-disc py-1 text-[18px] leading-[22px] font-bold">Panama</li>
                     </ul>
                     <div className=" justify-end -mt-48 hidden md:flex">
                         <Image src='/images/left-nft.png' alt='left-nft' height={280} width={300} layout='fixed' />
