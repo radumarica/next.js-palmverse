5749c7a56e3b9e9c3030217d8d6016b0b7a3d5d7
riz <rizwan.k.off@gmail.com>
2022-09-05 23:54:06 +0530

    test

--- pages/first-location.jsx
+++ pages/first-location.jsx
@@ -44,11 +44,11 @@ const FirstLocation = () => {
                     <h1 className="md:text-4xl text-[28px]  font-bold text-center  pb-4  ">Why Costa Rica?</h1>
                     <div className="text-[21px] relative     font-medium mt-4  text-[#ECF7FD]  "><div className="absolute z-50 -top-5 left-0 lg:-left-3" >
 
-                    </div><p className="text-[18px] leading-[21px] font-medium py-3 px-3 md:text-left"> Costa Rica was chosen as the first location considering multiple factors such as the year-round season and good market prices for hotel rooms. </p></div>
+                    </div><p className="text-[18px] leading-[21px] font-medium py-2 px-3 md:text-left"> Costa Rica was chosen as the first location considering multiple factors such as the year-round season and good market prices for hotel rooms. </p></div>
                     <p className="text-[18px] leading-[21px] font-medium py-2 px-3 md:text-left  ">Secondly, Costa Rica Is considered a  cryptocurrency paradise due to the widespread adoption of cryptocurrencies by its citizens. Many stores accept cryptocurrency as payment</p>
-                    <p className="text-[18px] leading-[21px] font-medium py-1 px-3 md:text-left  ">We will implement cryptocurrency payments at the front desk and on our website. </p>
-                    <p className="text-[18px] leading-[21px] font-medium py-1 px-3 md:text-left  ">Usual activities in Costa Rica are surfing, rafting, sailing, ATV tours, volcano tours, natural parks visits, hiking, turtle watching, horseriding, and many more. </p>
-                    <p className="text-[18px] leading-[21px] font-medium py-1 px-3 md:text-left  ">Possible 2nd and 3rd Palmverse locations, which are also crypto-friendly :</p>
+                    <p className="text-[18px] leading-[21px] font-medium py-2 px-3 md:text-left  ">We will implement cryptocurrency payments at the front desk and on our website. </p>
+                    <p className="text-[18px] leading-[21px] font-medium py-2 px-3 md:text-left  ">Usual activities in Costa Rica are surfing, rafting, sailing, ATV tours, volcano tours, natural parks visits, hiking, turtle watching, horseriding, and many more. </p>
+                    <p className="text-[18px] leading-[21px] font-medium py-2 px-3 md:text-left  ">Possible 2nd and 3rd Palmverse locations, which are also crypto-friendly :</p>
                     <ul className="pl-8 pb-8" >
                         <li className="list-disc py-1 text-[18px] leading-[21px] font-bold">Miami</li>
                         <li className="list-disc py-1 text-[18px] leading-[21px] font-bold">Portugal</li>
