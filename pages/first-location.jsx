8de967f2c7a63649e66661991d14ba824c3a72d9
riz <rizwan.k.off@gmail.com>
2022-09-05 23:47:15 +0530

    test

--- pages/first-location.jsx
+++ pages/first-location.jsx
@@ -18,7 +18,7 @@ const FirstLocation = () => {
                 <div className="pt-10">
                     <div className="relative  mx-auto bg-[#ECF7FD] text-black w-screen">
 
-                        <h1 className="md:text-4xl text-[28px]  font-bold px-3 text-center leading-9 ">1st Location - Tamarindo, Costa Rica</h1>
+                        <h1 className="md:text-4xl text-[28px]  font-medium px-3 text-center leading-9 ">1st Location - Tamarindo, Costa Rica</h1>
 
                     </div>
 
@@ -37,26 +37,27 @@ const FirstLocation = () => {
 
                         <Image src='/images/first_hotel.png' alt="first hotel" height={250} width={380} layout="fixed" />
                     </div>
-                    <h1 className="text-4xl font-bold px-3 pb-4 pt-10 ">Why Costa Rica?</h1>
+                    <div className="flex justify-center  md:hidden mt-4 ">
+                        <Image src='/images/left-nft.png' alt='left-nft' height={230} width={220} layout='fixed' />
+                    </div>
+
+                    <h1 className="md:text-4xl text-[28px]  font-bold text-center  pb-4  ">Why Costa Rica?</h1>
                     <div className="text-[21px] relative     font-medium mt-4  text-[#ECF7FD]  "><div className="absolute z-50 -top-5 left-0 lg:-left-3" >
 
-                    </div><p className="pl-4"><span className="w-4 "></span>  Costa Rica was chosen as the first location considering multiple factors such as the year-round season and good market prices for hotel rooms. </p></div>
-                    <p className="text-[18px] leading-[22px] font-medium py-3 px-3 md:text-left  ">Secondly, Costa Rica Is considered a  cryptocurrency paradise due to the widespread adoption of cryptocurrencies by its citizens. Many stores accept cryptocurrency as payment</p>
-                    <p className="text-[18px] leading-[22px] font-medium py-2 px-3 md:text-left  ">We will implement cryptocurrency payments at the front desk and on our website. </p>
-                    <p className="text-[18px] leading-[22px] font-medium py-2 px-3 md:text-left  ">Usual activities in Costa Rica are surfing, rafting, sailing, ATV tours, volcano tours, natural parks visits, hiking, turtle watching, horseriding, and many more. </p>
-                    <p className="text-[18px] leading-[22px] font-medium py-2 px-3 md:text-left  ">Possible 2nd and 3rd Palmverse locations, which are also crypto-friendly :</p>
-                    <ul className="pl-8" >
-                        <li className="list-disc py-1 text-[18px] leading-[22px] font-bold">Miami</li>
-                        <li className="list-disc py-1 text-[18px] leading-[22px] font-bold">Portugal</li>
-                        <li className="list-disc py-1 text-[18px] leading-[22px] font-bold">Panama</li>
+                    </div><p className="text-[18px] leading-[21px] font-medium py-3 px-3 md:text-left"> Costa Rica was chosen as the first location considering multiple factors such as the year-round season and good market prices for hotel rooms. </p></div>
+                    <p className="text-[18px] leading-[21px] font-medium py-2 px-3 md:text-left  ">Secondly, Costa Rica Is considered a  cryptocurrency paradise due to the widespread adoption of cryptocurrencies by its citizens. Many stores accept cryptocurrency as payment</p>
+                    <p className="text-[18px] leading-[21px] font-medium py-1 px-3 md:text-left  ">We will implement cryptocurrency payments at the front desk and on our website. </p>
+                    <p className="text-[18px] leading-[21px] font-medium py-1 px-3 md:text-left  ">Usual activities in Costa Rica are surfing, rafting, sailing, ATV tours, volcano tours, natural parks visits, hiking, turtle watching, horseriding, and many more. </p>
+                    <p className="text-[18px] leading-[21px] font-medium py-1 px-3 md:text-left  ">Possible 2nd and 3rd Palmverse locations, which are also crypto-friendly :</p>
+                    <ul className="pl-8 pb-8" >
+                        <li className="list-disc py-1 text-[18px] leading-[21px] font-bold">Miami</li>
+                        <li className="list-disc py-1 text-[18px] leading-[21px] font-bold">Portugal</li>
+                        <li className="list-disc py-1 text-[18px] leading-[21px] font-bold">Panama</li>
                     </ul>
                     <div className=" justify-end -mt-48 hidden md:flex">
                         <Image src='/images/left-nft.png' alt='left-nft' height={280} width={300} layout='fixed' />
                     </div>
 
-                    <div className="flex justify-end -mt-40 md:hidden ">
-                        <Image src='/images/left-nft.png' alt='left-nft' height={230} width={220} layout='fixed' />
-                    </div>
 
                 </div>
             </div>
