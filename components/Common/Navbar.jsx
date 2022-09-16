aa52dafe7329c9afffca5c63d0906e9d035b0742
radumarica <radunicolaemarica@gmail.com>
2022-09-16 19:23:38 +0300

    test

--- components/Common/Navbar.jsx
+++ components/Common/Navbar.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import Link from 'next/link'
 import { BsTwitter } from "react-icons/bs";
+
 const navs = [
     {
         name: 'Home',
@@ -28,11 +29,12 @@ const Header = ({ showDrop, setShowDrop, mobilNav, setMobilNav }) => {
 
 
     return <Navbar
+        className="darkbg"
         fluid={true}
         style={{ fontFamily: "'Exo 2', ' sans- serif'", backgroundColor: '#2A2A29', borderBottom: '1px solid gray', height: '100px', padding: "1rem 0.3rem" }}
     >
         <Navbar.Brand href="/">
-            <div className="hidden md:block">
+            <div className="hidden md:block tab-nav-logo">
 
                 <Image
                     src="/images/logo.png"
@@ -52,7 +54,7 @@ const Header = ({ showDrop, setShowDrop, mobilNav, setMobilNav }) => {
                     alt="Palmverse Logo"
                 />
             </div>
-            <span className=" whitespace-nowrap text-[32px] md:text-[40px] font-semibold pl-1  md:pl-2  text-[#ECF7FD]">
+            <span className="palm whitespace-nowrap text-[32px] md:text-[40px] font-semibold pl-1  md:pl-2  text-[#ECF7FD]">
                 PALMVERSE
             </span>
         </Navbar.Brand>
@@ -125,13 +127,13 @@ const Header = ({ showDrop, setShowDrop, mobilNav, setMobilNav }) => {
                     FAQ
                 </h3>
 
-                
+
             </Link>
 
             <a className="hover:text-[#13D511] text-lg text-[#ECF7FD] transition-all font-semibold cursor-pointer " rel="noreferrer" href="https://www.book-palmverse.com/" target='_blank'>
                 Book
 
-            </a> 
+            </a>
             <a className="hover:text-[#13D511] text-lg text-[#ECF7FD] transition-all font-semibold cursor-pointer pr-3 " href="https://twitter.com/palmverse" target='_blank' rel="noreferrer" >
                 <BsTwitter className="text-[#ECF7FD] text-2xl" />
 
