396c4cdeab7d7ce6cb5a95a63bfea374036bbf71
riz <rizwan.k.off@gmail.com>
2022-10-03 00:06:35 +0530

    mobilw update

--- components/Page/Home/Hero.jsx
+++ components/Page/Home/Hero.jsx
@@ -88,7 +88,7 @@ const Hero = () => {
           <div className="grid grid-cols-2  md:w-[80rem] md:grid md:grid-cols-3 md:gap-6 space-y-10 md:space-y-0 justify-evenly items-center md:py-5 py-10 w-[80%] mx-auto">
             <div className="flex flex-col items-center justift-center">
               <Image
-                src="/icons/3d/agreement2.png"
+                src="/bland/1-.png"
                 alt="PHYSICAL NFT HOTEL & HOSTEL"
                 height={156}
                 width={156}
@@ -103,7 +103,7 @@ const Hero = () => {
             </div>
             <div className="flex flex-col items-center justift-center">
               <Image
-                src="/icons/pay.png"
+                src="/bland/2 (1).png"
                 alt="PHYSICAL NFT HOTEL & HOSTEL"
                 height={156}
                 width={156}
@@ -118,7 +118,7 @@ const Hero = () => {
             </div>
             <div className="flex flex-col items-center justift-center">
               <Image
-                src="/icons/3d/money_bag2.png"
+                src="/bland/3 (1).png"
                 alt="PHYSICAL NFT HOTEL & HOSTEL"
                 height={156}
                 width={156}
@@ -134,7 +134,7 @@ const Hero = () => {
 
             <div className="flex flex-col pt-14  items-center justift-center">
               <Image
-                src="/icons/group.png"
+                src="/bland/4.png"
                 alt="PHYSICAL NFT HOTEL & HOSTEL"
                 height={156}
                 width={156}
@@ -149,7 +149,7 @@ const Hero = () => {
             </div>
             <div className="flex flex-col pt-14  items-center justift-center">
               <Image
-                src="/icons/3d/agreement2.png"
+                src="/bland/5.png"
                 alt="PHYSICAL NFT HOTEL & HOSTEL"
                 height={156}
                 width={156}
@@ -164,7 +164,7 @@ const Hero = () => {
             </div>
             <div className="flex flex-col pt-14  items-center justift-center">
               <Image
-                src="/icons/3d/agreement2.png"
+                src="/bland/6.png"
                 alt="PHYSICAL NFT HOTEL & HOSTEL"
                 height={156}
                 width={156}
@@ -370,11 +370,11 @@ const Hero = () => {
               delaySpeed={1000}
             /> <br /> Pay With Web3 Wallet
           </strong>
-          <p className="md:text-[32px] hidden md:flex text-[18px] text-center pb-5 mt-6 px-4 font-extralight md:w-9/12 text-[#ECF7FD]  ">
+          {/* <p className="md:text-[32px] hidden md:flex text-[18px] text-center pb-5 mt-6 px-4 font-extralight md:w-9/12 text-[#ECF7FD]  ">
             Palmverse builds technologies that help people connect IRL{" "}
             <br className="md:hidden" /> join communities, and bridge the web2
             travel experience to web3.
-          </p>
+          </p> */}
           <div className=" mt-5  md:mt-0 md:scale-100">
             <Image
               src="/images/laptop.png"
@@ -388,11 +388,72 @@ const Hero = () => {
               BETA VERSION IS <span className="text-green_text" >LIVE</span>
             </h1>
           </div>
-          <div className="w-[90%] overflow-x-auto md:hidden">
-            <p className="md:text-[32px]  text-[22px] text-center pb-10 pt-4  leading-7 font-light md:w-9/12 text-[#ECF7FD]  ">
-              Palmverse builds technologies that help people connect IRL, join
-              communities, and bridge the web2 travel experience to web3.
+          <div className="w-screen overflow-x-auto flex flex-col items-center justify-center ">
+            <p className="md:text-[40px]  text-[20px] text-center pt-10 font-bold md:w-9/12 text-[#ECF7FD]  ">
+              {"Welcome to the future of hospitality"
+                .split(" ")
+                .map((word, index) => (
+                  <span key={index}>{`${word} `}</span>
+                ))}
             </p>
+            <p className="md:text-[22px] pt-2 pb-8 mx-4 text-[16px] text-center  font-semibold md:w-9/12 text-[#ECF7FD]  ">
+              {"We make it easy to book your next trip directly from your web3 wallet."
+                .split(" ")
+                .map((word, index) => (
+                  <span key={index}>{`${word} `}</span>
+                ))}
+            </p>
+          </div>
+
+          <div className="w-screen overflow-x-auto flex flex-col items-center justify-center mt-10 ">
+            <p className="md:text-[40px]  text-[20px] text-center pt-10 font-bold md:w-9/12 text-[#ECF7FD]  ">
+              The PALMVERSE NFT Collection
+            </p>
+            <p className="md:text-[22px] pt-3 pb-8 text-[16px] px-4 text-center  font-semibold md:w-9/12 text-[#ECF7FD]  ">
+              We provide amazing utility and rewards for holders
+            </p>
+          </div>
+          <div className="flex flex-col justify-center space-y-4 items-center" >
+            <div className="mx-3  " >
+              <Image
+                src='/nfts/city (1).png'
+                alt="palmverse hotel"
+                width={230}
+                height={230}
+                layout="fixed"
+                className="rounded-md object-cover"
+              />
+            </div>
+            <div className="mx-3" >
+              <Image
+                src='/nfts/dead.png'
+                alt="palmverse hotel"
+                width={230}
+                height={230}
+                layout="fixed"
+                className="rounded-md object-cover"
+              />
+            </div>
+            <div className="mx-3  " >
+              <Image
+                src='/nfts/city.png'
+                alt="palmverse hotel"
+                width={230}
+                height={230}
+                layout="fixed"
+                className="rounded-md object-cover"
+              />
+            </div>
+            <div className="mx-3" >
+              <Image
+                src='/nfts/cool.png'
+                alt="palmverse hotel"
+                width={230}
+                height={230}
+                layout="fixed"
+                className="rounded-md object-cover"
+              />
+            </div>
           </div>
 
 
@@ -491,7 +552,23 @@ const Hero = () => {
 
           </div>
 
+          <div className="w-screen overflow-x-auto flex flex-col items-center justify-center mt-10 ">
+            <p className="md:text-[40px]  text-[20px] text-center pt-10 font-bold md:w-9/12 text-[#ECF7FD]  ">
+              We help properties access new <br /> Web3 customers  & communities
+            </p>
+            <p className="md:text-[22px] pt-3 pb-8 text-[16px] px-6 text-center  font-semibold md:w-9/12 text-[#ECF7FD]  ">
+              We make it easy to onboard properties into web3
+            </p>
+          </div>
+          <div className="flex justify-center" >
 
+            <Image
+              src="/images/uti.png"
+              alt="palmverse hotel"
+              width={1100}
+              height="600"
+            />
+          </div>
         </div>
         {/* <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 justify-evenly items-center w-[90%] mx-auto py-10 ">
           <div className="md:w-[50%] md:mx-auto md:px-4 scale-100 md:scale-100">
@@ -518,9 +595,9 @@ const Hero = () => {
             </span>
           </h1>
         </div> */}
-        <div className="grid md:grid-cols-2 gap-4 py-5 ">
+        <div className="grid md:grid-cols-2 gap-4 pb-5 pt-10 ">
           <div className="flex flex-col">
-            <h1 className="text-[24px] font-bold text-[#ECF7FD]  text-center pt-6">
+            <h1 className="text-[26px] font-bold text-[#ECF7FD]  text-center pt-6">
               NFT UTILITIES
             </h1>
             <div className="grid grid-cols-1  gap-4 pt-4">
