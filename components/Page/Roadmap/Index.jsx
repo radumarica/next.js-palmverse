f437db39509ede89a4b669aaf6e2374a9cab3209
riz <rizwan.k.off@gmail.com>
2022-10-04 18:44:42 +0530

    etts

--- components/Page/Roadmap/Index.jsx
+++ components/Page/Roadmap/Index.jsx
@@ -278,7 +278,7 @@ const Roadmap = () => {
                 <div style={{
                     backgroundImage: 'url(/images/roadmap/m-2.png)',
                     width: '100%',
-                    margin: '4px auto 0 auto',
+                    margin: '0 auto 0 auto',
                     backgroundSize: 'cover',
                     backgroundRepeat: 'no-repeat',
                     backgroundPosition: 'center',
